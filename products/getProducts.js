82ee302ad669ffc2589ac6003a81feb6c13e6aef
Bahar Işıklı <baharisikli@outlook.com>
2023-10-16 15:26:29 +0300

    update

--- products/getProducts.js
+++ products/getProducts.js
@@ -1,13 +1,22 @@
+const knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "123456789",
+    database: "orders",
+  },
+});
+
 module.exports = (req, res) => {
-  res.send(
-    knex("products")
-      .select("name", "price", "id")
-      .then((products) => {
-        res.json(products);
-      })
-      .catch((error) => {
-        console.error("database error:", error);
-        res.status(500).send("database error");
-      })
-  );
+  knex("products")
+    .select("name", "price", "id")
+    .then((products) => {
+      res.json(products);
+    })
+    .catch((error) => {
+      console.error("database error:", error);
+      res.status(500).send("database error");
+    });
 };
